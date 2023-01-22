adf09f734f1b28537073100f46e212a5130d9450
polimatilda <polimatilda@gmail.com>
2023-01-22 19:01:41 +0100

    title fix

--- konyv-kemping/src/pages/KempingMain.jsx
+++ konyv-kemping/src/pages/KempingMain.jsx
@@ -97,12 +97,16 @@ const guilds = [
 function KempingMain() {
   return (
     <Container fluid>
+      <Row className='mb-3'>
+        <Col>
+          <h1 className="text-center">Kedves Kalandor!</h1>
+        </Col>
+      </Row>
       <Row className="d-flex align-items-center">
         <Col lg>
-          <img src={logo} className="img-fluid rounded" alt="konyv kemping logo" />
+          <img src={logo} className="img-fluid rounded mb-3" alt="konyv kemping logo" />
         </Col>
         <Col lg>
-          <h1 className="text-center mt-5 mb-5">Kedves Kalandor!</h1>
           <p>Üdvözöllek a Nyírfa generációs űrhajó fedélzetén. Sokad magaddal szálltál fel erre a járműre, hogy eddig ismeretlen világokat járj be szerte a galaxisban. Egy ekkora gépet azonban nem könnyű fenntartani, ráadásul hosszú az út: fontos, hogy mindenki értsen valamihez. Ahhoz, hogy zökkenőmentes legyen az utazás, mesteremberekre lesz szükség, és mindenki kiválaszthatja, hogy mely területen szeretne tanulni. Ahhoz, hogy mesteremberré válj, <strong>5 történetet (ebből háromnak könyvnek vagy kisregénynek kell lennie [képregények és képes könyvek is számítanak!]) kell elolvasnod a céhed szempontjainak megfelelően. A pontok nem összevonhatóak.</strong></p>
           <p>Ha nem tudsz választani akkor lehet egy fő munkád, és mellette jártassá válhatsz egy másikban is. Ebben az esetben  <strong>6 történetet kell elolvasnod (ebből háromnak könyvnek vagy kisregénynek kell lennie [képregények és képes könyvek is számítanak!]): 4 történet tartozik a “fő” mesterségedhez, kettő pedig bármely másik általad választott mesterséghez.</strong></p>
         </Col>
@@ -128,7 +132,7 @@ function KempingMain() {
       </Row>
       <Row>
         <Col>
-        <h4 className='text-center my-3'>Céhek:</h4>
+          <h4 className='text-center my-3'>Céhek:</h4>
         </Col>
       </Row>
       <Row md={1} sm={1} xs={1} lg={2} className="my-4">
