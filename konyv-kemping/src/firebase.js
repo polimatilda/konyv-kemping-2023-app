d4bb8491b551da665e2e1593c799bc51a8a8064d
polimatilda <polimatilda@gmail.com>
2023-01-28 10:38:11 +0100

    firebase hosting deploy

--- konyv-kemping/src/firebase.js
+++ konyv-kemping/src/firebase.js
@@ -2,13 +2,23 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_API_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_API_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_API_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_API_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_API_APP_ID
+
+// }
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_API_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_API_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_API_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_API_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_API_APP_ID
+  apiKey: "AIzaSyCEplzJcikeKCgzSiW_JII7aNvs63uIv7Y",
+  authDomain: "konyv-kemping.firebaseapp.com",
+  projectId: "konyv-kemping",
+  storageBucket: "konyv-kemping.appspot.com",
+  messagingSenderId: "617911695196",
+  appId: "1:617911695196:web:adf6b7f5cae8cf0cce3399"
 
 }
 
