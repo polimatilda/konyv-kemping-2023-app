62c61132a23addcab64d9212f65168dfb503365f
polimatilda <polimatilda@gmail.com>
2023-01-28 15:21:43 +0100

    event button fix

--- konyv-kemping/src/components/Event.jsx
+++ konyv-kemping/src/components/Event.jsx
@@ -4,22 +4,22 @@ import { Col, Card, Button } from 'react-bootstrap'
 function Event({ event }) {
 
   return (
-      <Col className='my-3'>
-        <Card>
-          <Card.Header className='text-white'>{event.date} {event.time}</Card.Header>
-          <Card.Body>
-            <Card.Title>{event.name}</Card.Title>
-            <Card.Text>
-              {event.where}
-            </Card.Text>
-            <a href={event.link} target="_blank" rel='noreferrer'>
-              <Button variant="primary" disabled={event.link === "" ? true : false}>
-                Ugrás az eseményre
-              </Button>
+    <Col className='my-3'>
+      <Card>
+        <Card.Header className='text-white'>{event.date} {event.time}</Card.Header>
+        <Card.Body>
+          <Card.Title>{event.name}</Card.Title>
+          <Card.Text>
+            {event.where}
+          </Card.Text>
+          <Button variant="primary" disabled={event.link === "" ? true : false}>
+            <a href={event.link} target="_blank" rel='noreferrer' className='event-link'>
+              Ugrás az eseményre
             </a>
-          </Card.Body>
-        </Card>
-      </Col>
+          </Button>
+        </Card.Body>
+      </Card>
+    </Col>
   )
 }
 
